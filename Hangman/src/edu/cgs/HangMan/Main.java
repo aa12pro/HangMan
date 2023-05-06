5fe7c92b7e2f6e09a3a1d5bf0dfcb5bc67d990a5
aa12pro <sb737sundays@gmail.com>
2023-05-07 00:38:22 +0300

    Added Comments to make the code more understandable to anyone who has no java knowledge. Added more words in the word list renamed variables to match what they do.

--- Hangman/src/edu/cgs/HangMan/Main.java
+++ Hangman/src/edu/cgs/HangMan/Main.java
@@ -1,28 +1,36 @@
 package edu.cgs.HangMan;
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-import java.awt.event.ActionEvent;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
-
 	private JPanel contentPane;
-	private String[] words = {"ΓΛΙΣΤΡΩ", "ΑΜΕΙΒΩ", "ΒΟΡΙΝΟΣ", "ΔΙΑΡΡΟΗ", "ΕΚΠΑΓΛΟΣ", "ΚΛΥΣΜΑ", "ΙΛΙΓΓΟΣ", "ΟΙΟΝΕΙ", "ΧΝΟΤΟ", "ΣΥΜΦΥΗΣ", "ΩΣΟΤΟΥ"};
+	//Σύνολο των λέξεων
+	private String[] words = {"ΓΛΙΣΤΡΩ", "ΑΜΕΙΒΩ", "ΒΟΡΙΝΟΣ", "ΔΙΑΡΡΟΗ", "ΕΚΠΑΓΛΟΣ", "ΚΛΥΣΜΑ", "ΙΛΙΓΓΟΣ", "ΟΙΟΝΕΙ",
+			"ΧΝΟΤΟ", "ΣΥΜΦΥΗΣ", "ΩΣΟΤΟΥ", "ΑΛΦΑΒΗΤΟ", "ΜΑΘΗΜΑΤΙΚΑ", "ΕΞΥΠΝΟΣ", "ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΣ", "ΚΛΗΡΟΝΟΜΙΑ", "ΑΡΙΣΤΕΙΑ", 
+			"ΠΑΡΑΔΕΙΓΜΑ", "ΣΥΝΕΡΓΑΣΙΑ", "ΕΠΙΤΥΧΙΑ", "ΚΑΛΛΙΤΕΧΝΙΑ", "ΔΗΜΙΟΥΡΓΙΚΟΤΗΤΑ", "ΕΥΦΥΙΑ", "ΑΝΑΚΑΛΥΨΗ", "ΠΕΙΘΩ", "ΑΝΔΡΕΙΑ",
+			"ΑΙΣΙΟΔΟΞΙΑ", "ΕΠΙΜΟΝΗ", "ΠΕΙΡΑΜΑΤΙΣΜΟΣ", "ΦΙΛΟΔΟΞΙΑ", "ΑΝΑΠΤΥΞΗ", "ΑΝΤΟΧΗ", "ΑΥΤΟΠΕΠΟΙΘΗΣΗ", "ΑΞΙΟΠΡΕΠΕΙΑ", "ΚΑΙΝΟΤΟΜΙΑ"};
 	private String word;
-	private JLabel[] label;
-	JComboBox comboBox;
-	JLabel HangMan;
-	private int x = 50, y = 100, height = 200, width = 200;
+	//Κενά για την κάθε λέξη που μετά γίνονται τα γράμματα
+	private JLabel[] spaces;
+	//Σύνολο των γραμμάτων
+	private JComboBox<String> letters;
+	//Εικόνες του κρεμασμένου
+	private JLabel HangedMan;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,96 +45,108 @@ public class Main extends JFrame {
 		});
 	}
 
+	//Ότι φαίνεται στο παράθυρο που ανοίγει
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 601, 470);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(26, 95, 180));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η", "Θ", "Ι", "Κ", "Λ", "Μ", "Ν", "Ξ", "Ο", "Π", "Ρ", "Σ", "Τ", "Υ", "Φ", "Χ", "Ψ", "Ω"}));
-		comboBox.setBounds(316, 52, 89, 22);
-		contentPane.add(comboBox);
+		letters = new JComboBox();
+		letters.setModel(new DefaultComboBoxModel(new String[] {"Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η", "Θ", "Ι", "Κ", "Λ", "Μ", "Ν", "Ξ", "Ο", "Π", "Ρ", "Σ", "Τ", "Υ", "Φ", "Χ", "Ψ", "Ω"}));
+		letters.setBounds(491, 51, 73, 34);
+		contentPane.add(letters);
 		
+
 		Random random = new Random();
 		
+		//Διαλέγει την λέξη αφού παίρνει έναν τυχαίο αριθμό από το 0 μέχρι το άθροισμα των λέξεων σε αυτή τη λίστα "words"
 		word = words[random.nextInt(words.length)];
 		
-		label = new JLabel[word.length()];
+		spaces = new JLabel[word.length()];
 		
-		HangMan = new JLabel();
-		HangMan.setIcon(Constants.Default);
-		HangMan.setBounds(12, 0, 271, 169);
-		contentPane.add(HangMan);
+		HangedMan = new JLabel();
+		//Βάζει την εικόνα της κρεμάλας
+		HangedMan.setIcon(Constants.Default);
+		//Βάζει τις διαστάσεις και τη θέση της εικόνας
+		HangedMan.setBounds(12, 12, 269, 390);
+		contentPane.add(HangedMan);
 		
-		int x = 50, y= 100, height = 200, width = 200;
+		int x = 298, y = 73, height = 300, width = 400;
 		
 		for (int i = 0; i < word.length(); i++) {
-			label[i] = new JLabel();
-			label[i].setText("_");
-			label[i].setBounds(x, y, height, width);
-			contentPane.add(label[i]);
-			x+=10;
+			spaces[i] = new JLabel();
+			spaces[i].setText("_");
+			spaces[i].setFont(new Font("Georgia", Font.PLAIN, 30));
+			spaces[i].setBounds(x, y, height, width);
+			contentPane.add(spaces[i]);
+			x+=30;
 		}
 		
 		JButton btnChoose = new JButton("Choose");
 		btnChoose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (word.contains((CharSequence) comboBox.getSelectedItem())) {
+				if (word.contains((CharSequence) letters.getSelectedItem())) {
 					for (int i = 0; i < word.length(); i++) {
-						if (word.charAt(i) == comboBox.getSelectedItem().toString().charAt(0)) {
-							label[i].setText((String) comboBox.getSelectedItem());
+						if (word.charAt(i) == letters.getSelectedItem().toString().charAt(0)) {
+							spaces[i].setText((String) letters.getSelectedItem());
 						}
 					}
 					boolean allFilled = true;
 				    for (int i = 0; i < word.length(); i++) {
-				        if (label[i].getText().equals("_")) {
+				        if (spaces[i].getText().equals("_")) {
 				            allFilled = false;
 				            break;
 				        }
 				    }
+				    //Αν όλα τα κενά έχουν συμπληροθεί ο παίκτης νίκησε
 				    if (allFilled) {
 				        JOptionPane.showMessageDialog(null, "You won! Congratulations!");
 				        restart();
 				    }
 				} else {
-					if (HangMan.getIcon().equals(Constants.Default)) {
-						HangMan.setIcon(Constants.Stage_1);
-					} else if (HangMan.getIcon().equals(Constants.Stage_1)) {
-						HangMan.setIcon(Constants.Stage_2);
-					} else if (HangMan.getIcon().equals(Constants.Stage_2)) {
-						HangMan.setIcon(Constants.Stage_3);
-					} else if (HangMan.getIcon().equals(Constants.Stage_3)) {
-						HangMan.setIcon(Constants.Stage_4);
-					} else if (HangMan.getIcon().equals(Constants.Stage_4)) {
-						HangMan.setIcon(Constants.Stage_5);
-					} else if (HangMan.getIcon().equals(Constants.Stage_5)) {
-						HangMan.setIcon(Constants.Stage_6);
-					} else if (HangMan.getIcon().equals(Constants.Stage_6)) {
-						HangMan.setIcon(Constants.Stage_7);
-					} else if (HangMan.getIcon().equals(Constants.Stage_7)) {
-						HangMan.setIcon(Constants.Stage_8);
+					//Αλλάζει τις εικόνες ανάλογα με το στάδιο που βρίσκονται
+					if (HangedMan.getIcon().equals(Constants.Default)) {
+						HangedMan.setIcon(Constants.Stage_1);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_1)) {
+						HangedMan.setIcon(Constants.Stage_2);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_2)) {
+						HangedMan.setIcon(Constants.Stage_3);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_3)) {
+						HangedMan.setIcon(Constants.Stage_4);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_4)) {
+						HangedMan.setIcon(Constants.Stage_5);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_5)) {
+						HangedMan.setIcon(Constants.Stage_6);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_6)) {
+						HangedMan.setIcon(Constants.Stage_7);
+					} else if (HangedMan.getIcon().equals(Constants.Stage_7)) {
+						HangedMan.setIcon(Constants.Stage_8);
 					} else {
+						//Άμα τελιώσουν η φάσεις της εικόνας ο παίκτης χάνει
 						JOptionPane.showMessageDialog(null, "You lost! Better Luck Next Time!");
 						restart();
 					}
 				}
-				comboBox.removeItemAt(comboBox.getSelectedIndex());
+				//Βγάζει από το σύνολο των γραμμάτων το γράμμα που έχει ήδη επιλεχτεί
+				letters.removeItemAt(letters.getSelectedIndex());
 			}
 		});
-		btnChoose.setBounds(316, 96, 89, 23);
+		btnChoose.setBounds(491, 102, 89, 23);
 		contentPane.add(btnChoose);
 	}
 	
+	//Είτε κλείνει και τελιώνει το παιχνίδι ή το ξανααρχίζει
 	public void restart() {
 		int restart = JOptionPane.showOptionDialog(this, "Select an option:", null, JOptionPane.YES_NO_OPTION,
 				JOptionPane.INFORMATION_MESSAGE, null, Constants.RESTART_OPTIONS, -1);
 		if (restart == 0) {
-			//this.dispose();
-			new Main();
+			this.dispose();
+			new Main().setVisible(true);
 		} else {
 			System.exit(0);
 		}
